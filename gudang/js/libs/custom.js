afa62d61337733366a2dda3fea996a5450e86081
agusdiyansyah <kreasiin@gmail.com>
2015-06-16 17:37:47 +0700

    gudang

--- gudang/js/libs/custom.js
+++ gudang/js/libs/custom.js
@@ -2,21 +2,32 @@ var expand = true;
 
 // default
 $(".content").load('statistic');
-
 function link (module, title) {
 	$.ajax({
 		url: module,
 		cache: false,
 		dataType: 'html',
+		beforeSend: function() {
+			$('#myModal').modal({
+			    backdrop: 'static',
+			    keyboard: false
+			})
+		},
 		success:function(html) {
+			$('#myModal').modal('hide');
 			$("#wrapper").css('padding-left', '225px');
 			$(".side-nav").css('width', '225px');
 			$(".side-nav span").css('display', 'inline-block');
 			$(".content").html(html);
 			$(".title").html(title);
 		}
+	})
+	.fail(function() {
+		alert( "error" );
+		console.log("error");
 	});
 }
+
 
 $(".menu").bind('click', function() {
 	if (expand) {
